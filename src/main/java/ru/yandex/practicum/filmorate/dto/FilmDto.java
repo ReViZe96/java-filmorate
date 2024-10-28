921f5ac53d2af67d447e47f442772a9d8e42cf96
Artem Kasyanov <mr.revize.com@gmail.com>
2024-10-28 22:26:01 +0300

    filmorate 0.3.7: code-style - unused import of Mpa in FilmDto

--- src/main/java/ru/yandex/practicum/filmorate/dto/FilmDto.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/FilmDto.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.dto;
 
 import lombok.Builder;
 import lombok.Data;
-import ru.yandex.practicum.filmorate.model.Mpa;
 
 import java.time.LocalDate;
 import java.util.List;
